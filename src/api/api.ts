6e4dbc00dad6022bca9ff0fda6c2cc9571f46bb8
Petr <zhirkovpetr95@gmail.com>
2024-03-17 01:32:47 +0300

    refactor: refactor a project on gh pages

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import { IProducts } from '../store/products/products-slice.ts';
 
 export const productsAPI = {
   async getProducts(): Promise<IProducts[]> {
-    const res = await fetch('src/assets/data.json');
+    const res = await fetch('assets/data.json');
     return await res.json();
   },
 };
